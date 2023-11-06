87b6c41c371b3589745cf68039ba977b5fa5121e
eoloughlin05 <eoloughlin05@qub.ac.uk>
2023-11-06 21:49:41 +0000

    4.2 make a method return a spawn point

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,14 +9,19 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
-        float spawnPosX = Random.Range(spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation); 
     }
 
     void Update()
     {
         
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
     }
 }
